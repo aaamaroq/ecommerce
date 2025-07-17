ae27d47f2dc2dde07f3863560db295185dc873fd
Adrián Amaro <adrianamaroquero@gmail.com>
2025-07-17 20:48:38 +0200

    [update] split logic for product

--- ApiEcommerce/src/main/java/com/ecommerce/product/adapter/controller/ProductController.java
+++ ApiEcommerce/src/main/java/com/ecommerce/product/adapter/controller/ProductController.java
@@ -1,16 +1,13 @@
 package com.ecommerce.product.adapter.controller;
 
-
 import com.ecommerce.product.adapter.dto.ProductRequest;
+import com.ecommerce.product.adapter.messaging.KafkaProductPublisher;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private KafkaTemplate<String, ProductRequest> kafkaTemplate;
+    private final KafkaProductPublisher kafkaProductPublisher;
 
-    @Value("${spring.kafka.topic.request-info}")
-    private String topic;
+    public ProductController(KafkaProductPublisher kafkaProductPublisher) {
+        this.kafkaProductPublisher = kafkaProductPublisher;
+    }
 
     @GetMapping("/getProducto")
     public ResponseEntity<String> getProducto(
@@ -34,20 +31,16 @@ public class ProductController {
             @RequestParam("email") @Email(message = "Email inválido") String email) {
 
         try {
-            // Validar los parámetros aquí antes de crear el objeto ProductRequest
             ProductRequest productRequest = new ProductRequest(nombre, email, id);
 
-            // Log para conocer el producto
             log.info("Producto solicitado: {}", productRequest);
 
-            kafkaTemplate.send(topic, productRequest);
+            kafkaProductPublisher.publishProductRequest(productRequest);
 
-            // Log para confirmar envío a Kafka
             log.info("Mensaje enviado a Kafka para el producto: {}", productRequest);
 
             return ResponseEntity.ok("Solicitud de producto enviada");
         } catch (Exception e) {
-            // Manejo de excepciones
             log.error("Error al procesar la solicitud", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al procesar la solicitud");
         }
